183cbf8cd4cfdf616b6511e70f839eddea5710ff
Joey Groh <jgroh@phase2technology.com>
2014-12-11 17:24:43 -0500

    Work for hero, homepage pillars

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,23 @@
-$(document).ready(function() {
-	$('body').addClass("TEST");
-});
+(function(w){
+
+  $(function() {
+    $( "#tabs" ).tabs();
+  });
+
+  //$(function() {
+  //  var $container = $('#content-blocks--masonry');
+  //  // initialize
+  //  $container.masonry({
+  //    columnWidth: 300,
+  //    "isFitWidth": true,
+  //    itemSelector: '.content-blocks__masonry__item'
+  //  });
+  //
+  //  var $container = $('#content-blocks--masonry').masonry();
+  //  // layout Masonry again after all images have loaded
+  //  $container.imagesLoaded( function() {
+  //    $container.masonry();
+  //  });
+  //});
+
+})(this);
