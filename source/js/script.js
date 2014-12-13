43e9af88916b1092ea155881fafdb1da76634548
Joey Groh <jgroh@phase2technology.com>
2014-12-13 16:05:31 -0500

    Finishing homepage news

--- source/js/script.js
+++ source/js/script.js
@@ -1,47 +1,109 @@
-(function(w){
-
+(function (w) {
   $(function() {
-    //$( "#tabs" ).tabs();
+    "use strict";
+
+    $(window).setBreakpoints({
+      // use only largest available vs use all available
+      distinct: true,
+      // array of widths in pixels where breakpoints
+      // should be triggered
+      breakpoints: [
+        300,
+        480,
+        640,
+        768,
+        1024,
+        1190
+      ]
+    });
+
+    /**
+     * Initialize tabs/accordian for pillars
+     */
+
+    $('#pillar_tabs').easyResponsiveTabs({
+      type: 'default', //Types: default, vertical, accordion
+      width: 'auto', //auto or any width like 600px
+      fit: true,   // 100% fit in a container
+      closed: 'accordion', // Start closed if in accordion view
+      activate: function (event) { // Callback function if tab is switched
+        //var $tab = $(this);
+        //var $info = $('#tabInfo');
+        //var $name = $('span', $info);
+
+        //$name.text($tab.text());
+
+        //$info.show();
+      }
+    });
+
+    var $container = $('#post-filter-results');
+    // initialize
+    $container.masonry({
+      columnWidth: 290,
+      "isFitWidth": true,
+      itemSelector: '#post-filter-results li'
+    });
+
+    // layout Masonry again after all images have loaded
+    $container.imagesLoaded( function() {
+      $container.masonry();
+    });
+
+    $(window).bind('enterBreakpoint300',function() {
+      $container.masonry({
+        columnWidth: 290
+      });
+    });
+
+    $(window).bind('enterBreakpoint480',function() {
+      $container.masonry({
+        columnWidth: 440
+      });
+    });
+
+    $(window).bind('enterBreakpoint640',function() {
+      $container.masonry({
+        columnWidth: 320
+      });
+    });
+
+    $(window).bind('enterBreakpoint768',function() {
+      $container.masonry({
+        columnWidth: 398
+      });
+    });
+
+    $(window).bind('enterBreakpoint1024',function() {
+      $container.masonry({
+        columnWidth: 340
+      });
+    });
+
+    $(window).bind('enterBreakpoint1190',function() {
+      $container.masonry({
+        columnWidth: 297
+      });
+    });
+
+    /**
+     * Initialize Select replacement
+     */
+
+    $('.js-selecter').selecter();
+
+    $(document).on( "click", "#post-filter-tags input", function() {
+      $('#post-filter-results > li').not('.more').hide();
 
 
-  $('#pillar_tabs').easyResponsiveTabs({
-    type: 'default', //Types: default, vertical, accordion
-    width: 'auto', //auto or any width like 600px
-    fit: true,   // 100% fit in a container
-    closed: 'accordion', // Start closed if in accordion view
-    activate: function(event) { // Callback function if tab is switched
-      //var $tab = $(this);
-      //var $info = $('#tabInfo');
-      //var $name = $('span', $info);
+      $('#post-filter-tags').find('input:checked').each(function () {
+        $('#post-filter-results > li.' + $(this).attr('rel')).show();
+      });
 
-      //$name.text($tab.text());
+      $container.masonry();
 
-      //$info.show();
-    }
-  });
-
-  });
-
-  //$('#verticalTab').easyResponsiveTabs({
-  //  type: 'vertical',
-  //  width: 'auto',
-  //  fit: true
-  //});
-
-  //$(function() {
-  //  var $container = $('#content-blocks--masonry');
-  //  // initialize
-  //  $container.masonry({
-  //    columnWidth: 300,
-  //    "isFitWidth": true,
-  //    itemSelector: '.content-blocks__masonry__item'
-  //  });
-  //
-  //  var $container = $('#content-blocks--masonry').masonry();
-  //  // layout Masonry again after all images have loaded
-  //  $container.imagesLoaded( function() {
-  //    $container.masonry();
-  //  });
-  //});
+      });
+    });
 
 })(this);
+
